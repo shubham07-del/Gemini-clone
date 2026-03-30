d552817fe56e979d00d8ff7d5ee2f3f4cc0d35af
Shubham Lenka <shubhamlenka43@gmail.com>
2026-03-31 01:01:27 +0530

    remove suggestion cards from home screen

--- src/components/main-content/GreetpCard.jsx
+++ src/components/main-content/GreetpCard.jsx
@@ -1,12 +1,10 @@
 import React from 'react'
 import Greet from './Greet'
-import Cards from './Cards'
 
 const GreetpCard = () => {
   return (
     <div>
       <Greet/>
-      <Cards/>
     </div>
   )
 }
